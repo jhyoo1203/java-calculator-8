898ea5c70a4c928471f35af5b5603c4af5e80041
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-10-18 16:28:35 +0900

    feat: 더하기 로직 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import calculator.config.AppConfig;
+import calculator.presentation.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig config = new AppConfig();
+        CalculatorController controller = config.calculatorController();
+
+        controller.plus();
     }
 }
